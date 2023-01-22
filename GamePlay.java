583f2c2eb7d99dda0cca99fab5be38be7850b0e9
WheezerAZ <122698494+WheezerAZ@users.noreply.github.com>
2023-01-21 17:22:08 -0700

    Updating files

--- GamePlay.java
+++ GamePlay.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 public class GamePlay {
     public static void main(String[] args) {
-        
+        private Person player = new Person();
+        String inputFirstName;
+        String inputLastName;
+        String lastNameCheck;
+        Scanner scnr = new Scanner(System.in);
+
+        // Lets Prompt the user for their name
+        System.out.println("What is your first name?");        
+        inputFirstName = scnr.nextLine();
+
+        // Check if they want enter their lastname?
+        System.out.println("Do you want to enter your lastname(Y/N)?");
+        lastNameCheck = scnr.nextLine();
+
+        if ( lastNameCheck.equals('Y') ) {
+            System.out.println("What is your last name?");        
+            inputLastName = scnr.nextLine();
+            player.setName(inputFirstName, inputLastName);
+        } else {
+            player.setName(inputFirstName);
+        }
+
+
+
     }
 }
